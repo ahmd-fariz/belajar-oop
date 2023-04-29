782fc6b5317eaafa431a928ffdc88865865398e4
unknown <ahmadfarizmaulana4@gmail.com>
2023-04-29 16:04:11 +0700

    update data kedua

--- bljr-oop-visibility.php
+++ bljr-oop-visibility.php
@@ -2,13 +2,11 @@
 
 class Produk
 {
-    public $judul,
-    $penulis,
-    $penerbit;
-
-    protected $diskon = 0;
-
-    private $harga;
+    private $judul,
+        $penulis,
+        $penerbit,
+        $diskon = 0,
+        $harga;
 
     public function __construct($judul, $penulis, $penerbit, $harga)
     {
@@ -18,10 +16,56 @@ class Produk
         $this->harga = $harga;
     }
 
+    public function setJudul($judul)
+    {
+        $this->judul = $judul;
+    }
+
+    public function getJudul()
+    {
+        return $this->judul;
+    }
+
+    public function setPenerbit($penerbit)
+    {
+        $this->penerbit = $penerbit;
+    }
+
+    public function getPenerbit()
+    {
+        return $this->penerbit;
+    }
+
+    public function setPenulis($penulis)
+    {
+        $this->penulis = $penulis;
+    }
+
+    public function getPenulis()
+    {
+        return $this->penulis;
+    }
+
+    public function setDiskon($diskon)
+    {
+        $this->diskon = $diskon;
+    }
+
+    public function getDiskon()
+    {
+        return $this->diskon;
+    }
+
+    public function setHarga($harga)
+    {
+        $this->harga = $harga;
+    }
+
     public function getHarga()
     {
         return $this->harga - ($this->harga * $this->diskon / 100);
     }
+
 
     public function getLabel()
     {
@@ -62,10 +106,6 @@ class Game extends Produk
         $this->waktuMain = $waktuMain;
     }
 
-    public function setDiskon($diskon)
-    {
-        return $this->diskon = $diskon;
-    }
 
     public function getInfoProduk()
     {
@@ -95,4 +135,6 @@ echo "<hr>";
 $produk2->setDiskon(50);
 echo $produk2->getHarga();
 
-?>
+echo "<hr>";
+$produk1->setPenulis("Fariz");
+echo $produk1->getPenulis();
